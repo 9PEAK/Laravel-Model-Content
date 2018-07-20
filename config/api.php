0f2b549b2ff1822daf21e2bf3520d10e1a160879
Kane <52653784@qq.com>
2018-07-20 10:12:23 +0800

    Start

--- config/api.php
+++ config/api.php
@@ -14,12 +14,16 @@ return [
 
 	'mws' => [
 		'US' => [
+			'access_key' => env('MWS_US'),
+			'secret_key' => 'EHzZ5BkA7iGimunPoK3GXGp+ELG26+UuR0Pn61Wl',
 			'application_name' => 'NIKKY HOME',
 			'application_version' => 1.0,
 			'seller_id' => 'A2TXXDJHIV03DN',
 		],
 
 		'UK' => [
+			'access_key' => env('MWS_UK'),
+			'secret_key' => '7rqP2NcaNeQbhLg8+TnzTG+ESY57Fsw8wcgZUwrb',
 			'application_name' => 'NIKKY HOME',
 			'application_version' => 1.0,
 			'seller_id' => 'A1Q37S3V245YBI',

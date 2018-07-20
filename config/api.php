30a52b52f966a886cc7387f44389d57da7073462
Kane <52653784@qq.com>
2018-07-20 10:08:34 +0800

    Start

--- config/api.php
+++ config/api.php
@@ -14,16 +14,12 @@ return [
 
 	'mws' => [
 		'US' => [
-			'access_key' => 'AKIAITFM4OR5J3ULFSHA',
-			'secret_key' => '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',
 			'application_name' => 'NIKKY HOME',
 			'application_version' => 1.0,
 			'seller_id' => 'A2TXXDJHIV03DN',
 		],
 
 		'UK' => [
-			'access_key' => 'AKIAI7JZUYRNM74RYG6A',
-			'secret_key' => '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',
 			'application_name' => 'NIKKY HOME',
 			'application_version' => 1.0,
 			'seller_id' => 'A1Q37S3V245YBI',
